fe8fa7898f435b45ba16a816a2662fcb4a3fe2c3
Nathinker <141727318+Nathinker@users.noreply.github.com>
2024-04-28 09:58:17 +0200

    Removed Jump Functionality
    Changelog:
    - Removed the code for jumping
    - Shortened an if else if statement in the Move Class

--- Platformer Game/Assets/Scripts/Move.cs
+++ Platformer Game/Assets/Scripts/Move.cs
@@ -6,9 +6,7 @@ using UnityEngine.InputSystem;
 public class Move : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3;
-    [SerializeField] private float jumpForce = 3;
     [SerializeField] private ContactFilter2D groundFilter;
-
     private Vector2 inputDir = Vector2.zero;
     private CapsuleCollider2D coll;
     private Rigidbody2D rb;
@@ -18,7 +16,7 @@ public class Move : MonoBehaviour
     public int playerFlipState = 0;
     public int gravityState = 0; 
     private bool isGrounded = false;
-    private bool isJumping = false;
+    private float dirY = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -34,45 +32,30 @@ public class Move : MonoBehaviour
     void Update()
     {
         bool isRunning = inputDir.x != 0;
-        
+
         // Checks the approperiate animaton for the actions of the player character
         anim.SetBool("Run", isRunning);
         anim.SetBool("Grounded", isGrounded);
-        float dirY = rb.velocity.y;
+
+        dirY = rb.velocity.y;
+        isGrounded = coll.IsTouching(groundFilter);
+
+        // Checks if the player has upwards or downnwards gravity and sets the approperiate parameters accordingly
         groundFilter.minNormalAngle = -80f * (gravityState == 0 ? -1 : gravityState == 1 ? 1 : -1);
         groundFilter.maxNormalAngle = -100f * (gravityState == 0 ? -1 : gravityState == 1 ? 1 : -1);
         anim.SetFloat("DirY", dirY * (gravityState == 0 ? 1 : gravityState == 1 ? -1 : 1));
         playerTransform.rotation = Quaternion.Euler(0, 0, 180f * (gravityState == 0 ? 0 : gravityState == 1 ? 1 : 0));
-        isGrounded = coll.IsTouching(groundFilter);
         sprite.flipX = (playerFlipState == 1) != (gravityState == 1);
         MovePlayer();
         GravitySwitch();
     }
 
-    // Moves the player
+    // Function to move the player
     private void MovePlayer()
     {
         Vector2 newVelocity = rb.velocity;
         newVelocity.x = inputDir.x * moveSpeed;
         rb.velocity = newVelocity;
-    }
-
-    // Checks if the player is grounded and jumps if they are
-    private void Jump()
-    {
-        if (isJumping && isGrounded)
-        {
-            isJumping = false;
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            if (gravityState == 0) // Checks if gravity is downward, and normalizes the isJumping force
-            {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            }
-            else if (gravityState == 1) // Checks if gravity is upward, and flips the isJumping force
-            {
-                rb.AddForce(Vector2.down * jumpForce, ForceMode2D.Impulse);
-            }
-        }
     }
 
     // Sets the move direction when the left or right keys are pressed, and flips the sprite if necessary
@@ -81,22 +64,10 @@ public class Move : MonoBehaviour
         inputDir = context.ReadValue<Vector2>();
         bool flipX = inputDir.x < 0;
 
-        if (inputDir.x == -1) // Checks if the player is facing left
-        {
-            playerFlipState = 1;
-        }
-        else if (inputDir.x == 1) // Checks if the player is facing right
-        {
-            playerFlipState = 0;
-        }
+        // Checks if the player is facing left or right and flips the sprite accordingly
+        playerFlipState = (inputDir.x == -1) ? 1 : (inputDir.x == 1) ? 0 : playerFlipState;
 
         sprite.flipX = flipX ^ (gravityState == 1);
-    }
-
-    // Actives isJumping when the jump key is pressed
-    public void ActivateJump(InputAction.CallbackContext context)
-    {
-        isJumping = context.started;
     }
 
     // Switches the gravity when the Q key is pressed
@@ -110,6 +81,7 @@ public class Move : MonoBehaviour
         }
     }
 
+    // Sets the player's respawn positioning
     public void RespawnPositioning()
     {
         gravityState = 0;
